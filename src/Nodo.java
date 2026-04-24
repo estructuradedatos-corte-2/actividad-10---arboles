2fa927c7bdc57016acdb39284529f4a915e74b0f
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-04-24 02:34:42 +0000

    creacion de clases y codigo nodo

---
+++ src/Nodo.java
@@ -0,0 +1,25 @@
+
+import java.util.List;
+
+public class Nodo {
+    
+    private String dato;
+    private Nodo siguiente;
+    private Nodo anterior;
+
+    List<Nodo> hijo;
+
+    public Nodo(){
+        this.siguiente= null;
+        this.anterior= null;
+        this.dato= dato;
+
+    }
+
+    public String getdato(){
+        return dato;
+    }
+
+     
+
+}
